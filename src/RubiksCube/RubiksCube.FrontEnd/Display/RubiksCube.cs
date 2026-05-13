e0b82a21eb44ab84510b65686df13a153ea4054e
Stevo John <stevehuwjohn@gmail.com>
2026-05-13 20:04:53 +0100

    WIP

--- src/RubiksCube/RubiksCube.FrontEnd/Display/RubiksCube.cs
+++ src/RubiksCube/RubiksCube.FrontEnd/Display/RubiksCube.cs
@@ -83,7 +83,7 @@ public sealed class RubiksCube : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.FromNonPremultiplied(20, 20, 20, 255));
 
         GraphicsDevice.RasterizerState = RasterizerState.CullNone;
         
