0df1a9c594ab0c299ce3df0b12fa6015412fa8ad
Stevo John <stevehuwjohn@gmail.com>
2026-05-16 14:53:26 +0100

    WIP

--- src/RubiksCube.Core/Models/Cube.cs
+++ src/RubiksCube.Core/Models/Cube.cs
@@ -21,14 +21,25 @@ public class Cube
     public void ApplyMove(Face face, Direction direction)
     {
         RotateFace(face, direction);
-        
+
         // TODO: RotateEdges(face, direction);
     }
 
     private void RotateFace(Face face, Direction direction)
     {
-        for (var i = 0; i < 4; i++)
+        var matrix = _faces[(int) face];
+
+        if (direction == Direction.Clockwise)
         {
+            (matrix[0, 0], matrix[2, 0], matrix[2, 2], matrix[0, 2]) = (matrix[2, 0], matrix[2, 2], matrix[0, 2], matrix[0, 0]);
+
+            (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[2, 1], matrix[1, 2], matrix[0, 1], matrix[1, 0]);
+        }
+        else
+        {
+            (matrix[0, 0], matrix[2, 0], matrix[2, 2], matrix[0, 2]) = (matrix[0, 2], matrix[0, 0], matrix[2, 0], matrix[2, 2]);
+
+            (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[0, 1], matrix[0, 1], matrix[2, 1], matrix[1, 2]);
         }
     }
 }
