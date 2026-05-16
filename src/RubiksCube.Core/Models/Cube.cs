95fe03bf0720a0a7b76bcccdf3b39077755647dc
Stevo John <stevehuwjohn@gmail.com>
2026-05-16 14:54:30 +0100

    WIP

--- src/RubiksCube.Core/Models/Cube.cs
+++ src/RubiksCube.Core/Models/Cube.cs
@@ -39,7 +39,7 @@ public class Cube
         {
             (matrix[0, 0], matrix[2, 0], matrix[2, 2], matrix[0, 2]) = (matrix[0, 2], matrix[0, 0], matrix[2, 0], matrix[2, 2]);
 
-            (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[0, 1], matrix[0, 1], matrix[2, 1], matrix[1, 2]);
+            (matrix[1, 0], matrix[2, 1], matrix[1, 2], matrix[0, 1]) = (matrix[0, 1], matrix[0, 1], matrix[2, 1], matrix[2, 1]);
         }
     }
 }
